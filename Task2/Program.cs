86872e3949968498fc8eab813be064cabe1592a8
Magomedali Gajiev <mag198421@gmail.com>
2023-12-24 19:49:10 +0300

    Добавил задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string binary = DecimalToBinary(number);
+            Console.WriteLine($"Двоичное представление числа {number}: {binary}");
+        }
+
+        static string DecimalToBinary(int number)
+        {
+            string binary = "";
+
+            while (number > 0)
+            {
+                int remainder = number % 2;
+                binary = remainder + binary;
+                number /= 2;
+            }
+
+            return binary;
+        }
+    }
+}
