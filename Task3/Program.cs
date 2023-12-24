9abc94efc5ac96fad0b28cdcb17c65f7d19dc274
Magomedali Gajiev <mag198421@gmail.com>
2023-12-24 20:26:51 +0300

    Добавил задание 3

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+// Найти в строке с текстом подстроку с числами (такая подстрока всего одна).
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку с текстом:");
+        var text = Console.ReadLine();
+
+        var numbersSubstring = FindNumbersSubstring(text);
+        Console.WriteLine($"Подстрока с числами: {numbersSubstring}");
+    }
+
+    static string FindNumbersSubstring(string text)
+    {
+        // Используем регулярное выражение для поиска подстроки с числами
+        var pattern = @"\d+";
+        Match match = Regex.Match(text, pattern);
+
+        // Если найдено совпадение, возвращаем найденную подстроку
+        if (match.Success)
+        {
+            return match.Value;
+        }
+
+        // Если совпадение не найдено, возвращаем пустую строку
+        return string.Empty;
+    }
+}
