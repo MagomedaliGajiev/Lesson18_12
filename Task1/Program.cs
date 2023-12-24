0b5799e445b4acc78e95336b1882ba8248c78d40
Magomedali Gajiev <mag198421@gmail.com>
2023-12-24 19:43:47 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            for (int i = 0; i <= 1000
+
+            Console.WriteLine($"Количество чисел от 0 до 1000, делящихся на 3 без остатка: {count}");
+        }
+    }
+}
